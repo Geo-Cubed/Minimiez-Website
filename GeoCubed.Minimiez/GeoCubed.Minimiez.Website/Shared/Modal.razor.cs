69349a771fe7a154c9cd99b691ac9f5592931f78
George Paddon <sggpaddo@liverpool.ac.uk>
2021-08-20 22:28:45 +0100

    Added modal for shared usage

---
+++ GeoCubed.Minimiez/GeoCubed.Minimiez.Website/Shared/Modal.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GeoCubed.Minimiez.Website.Shared
+{
+    public partial class Modal
+    {
+        [Parameter]
+        public RenderFragment Title { get; set; }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+        private string _modalDisplay = "none;";
+        private string _modalClass = "";
+        private bool _showBackdrop = false;
+
+        public void Open()
+        {
+            this._modalDisplay = "block;";
+            this._modalClass = "show";
+            this._showBackdrop = true;
+        }
+
+        public void Close()
+        {
+            this._modalDisplay = "none";
+            this._modalClass = "";
+            this._showBackdrop = false;
+        }
+    }
+}
