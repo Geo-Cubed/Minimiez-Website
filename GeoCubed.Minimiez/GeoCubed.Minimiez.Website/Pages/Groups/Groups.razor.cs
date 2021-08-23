8443be79489e160027e1acc107c98b69ebacf7d1
George Paddon <sggpaddo@liverpool.ac.uk>
2021-08-23 18:28:08 +0100

    Started work on displaying the group stage

--- GeoCubed.Minimiez/GeoCubed.Minimiez.Website/Pages/Groups/Groups.razor.cs
+++ GeoCubed.Minimiez/GeoCubed.Minimiez.Website/Pages/Groups/Groups.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeoCubed.Minimiez.Website.Models.Groups;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,9 +8,127 @@ namespace GeoCubed.Minimiez.Website.Pages.Groups
 {
     public partial class Groups
     {
+        protected List<GroupsViewModel> groups { get; set; }
+            = new List<GroupsViewModel>();
+
         protected override async Task OnInitializedAsync()
         {
             // Get group data.
+            this.groups = new List<GroupsViewModel>()
+            {
+                new GroupsViewModel()
+                {
+                    Name = "1",
+                    GroupTeams = new List<GroupTeams>()
+                    {
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 1",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 2",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 3",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        }
+                    }
+                },
+                new GroupsViewModel()
+                {
+                    Name = "2",
+                    GroupTeams = new List<GroupTeams>()
+                    {
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 1",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 2",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 3",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        }
+                    }
+                },
+                new GroupsViewModel()
+                {
+                    Name = "3",
+                    GroupTeams = new List<GroupTeams>()
+                    {
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 1",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 2",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 3",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        }
+                    }
+                },
+                new GroupsViewModel()
+                {
+                    Name = "4",
+                    GroupTeams = new List<GroupTeams>()
+                    {
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 1",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 2",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        },
+                        new GroupTeams()
+                        {
+                            TeamName = "Team 3",
+                            Wins = 1,
+                            Loses = 2,
+                            Score = 3
+                        }
+                    }
+                }
+            };
         }
     }
 }
---
+++ GeoCubed.Minimiez/GeoCubed.Minimiez.Website/Shared/Card.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GeoCubed.Minimiez.Website.Shared
+{
+    public partial class Card
+    {
+        [Parameter]
+        public RenderFragment Headder { get; set; }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+    }
+}
