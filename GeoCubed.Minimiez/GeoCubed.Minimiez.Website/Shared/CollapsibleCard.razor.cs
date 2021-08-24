75b12ffa22b1d9806874960a5379414e6ac2ff33
George Paddon <sggpaddo@liverpool.ac.uk>
2021-08-24 18:57:16 +0100

    Team table of players done

---
+++ GeoCubed.Minimiez/GeoCubed.Minimiez.Website/Shared/CollapsibleCard.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeoCubed.Minimiez.Website.Shared
+{
+    public partial class CollapsibleCard
+    {
+        [Parameter]
+        public RenderFragment Header { get; set; }
+
+        [Parameter]
+        public RenderFragment Body { get; set; }
+
+        [Parameter]
+        public RenderFragment Footer { get; set; }
+
+        [Parameter]
+        public string ClassStyle { get; set; }
+
+        [Parameter]
+        public string ExtraStyles { get; set; }
+
+        protected bool IsCollapsed { get; set; }
+            = true;
+
+        protected string CollapsedStyle => this.IsCollapsed ? "collapse" : null;
+
+        protected void ToggleCollapsed()
+        {
+            this.IsCollapsed = !this.IsCollapsed;
+        }
+    }
+}
